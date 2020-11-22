0e3839aa8367a26852fe202392f30a1d80911fcb
José Miguel Ramírez <34416160+JMiguelRamirez@users.noreply.github.com>
2020-11-22 12:31:22 +0200

    Update to get two correlation plots

--- data_exploration.R
+++ data_exploration.R
@@ -1,17 +1,21 @@
 source('init.r')
 load_data(type="all")
 
-ships <- ships[,-8] #removing DELV
+ships <- ships[,-c(8,99)] #removing DELV
 
 # Removing variables with more than 25% NAs
 n <- nrow(ships) 
 ships <- ships[,sapply(ships, function(x) sum(length(which(is.na(x))))) < n*0.25] #from 140 to 120
 
-# Correlation plot
+# Correlation plots
 library(corrplot)
-correlation <- cor(ships[,4:50], use = "complete.obs")
-png(file="images/correlation_plot.png")
+correlation <- cor(ships[,c(4:50, 119)], use = "complete.obs")
+png(file="images/correlation_plot_1.png")
 corrplot(correlation, method="circle", tl.col = "black", tl.cex = 0.75)
 dev.off()
 
+correlation <- cor(ships[,c(4, 51:119)], use = "complete.obs")
+png(file="images/correlation_plot_2.png")
+corrplot(correlation, method="circle", tl.col = "black", tl.cex = 0.75)
+dev.off()
 

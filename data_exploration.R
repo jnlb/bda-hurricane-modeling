def9bcdccba307334f6bc79d2d14eb6771b4f83e
José Miguel Ramírez <34416160+JMiguelRamirez@users.noreply.github.com>
2020-12-12 13:22:49 +0200

    Updating data exploration

--- data_exploration.R
+++ data_exploration.R
@@ -1,6 +1,6 @@
 source('init.r')
 load_data(type="all", target="delta")
-
+library(corrplot)
 #ships <- ships[,-c(8,99)] #removing DELV
 
 # Removing variables with more than 25% NAs
@@ -13,4 +13,9 @@ correlation <- cor(ships[,indexes], use = "complete.obs")
 png(file="images/corrplot_delta_small.png", width = 500, height = 500)
 corrplot(correlation, method="circle", tl.col = "black", 
          tl.cex = 1.5, cl.cex = 1.25, cl.pos = "b")
+dev.off()
+
+pdf(file="images/corrplot_delta_small_presentation.pdf", width = 5, height = 5)
+corrplot(correlation, method="circle", tl.col = "black", 
+         tl.cex = 1.75, cl.cex = 1.5, cl.align.text = "l")
 dev.off()
